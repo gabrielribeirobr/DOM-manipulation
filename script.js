20185ee4e64727eb3d17e98b09e9c280ae2bf62c
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-05-16 18:24:39 +0000

    button remove items for list work

--- script.js
+++ script.js
@@ -6,11 +6,20 @@ btn.addEventListener("click", () => {
 const btnAdditems = document.getElementById("btnAdditems");
 btnAdditems.addEventListener("click", () => {
     const li = document.createElement("li");    // criamos o elemento <li>
+    li.classList.add("itemList");               // denominamos "itemList" como nome da classe
     li.textContent = "New item";                // nomeamos o item da lista
     ul.appendChild(li);                         // Adiciona o <li> a <ul>
 })
 
 const btnRemoveList = document.getElementById("btnRemoveList");
 btnRemoveList.addEventListener("click", () => {
-    ul.remove("ul");
+    ul.remove();
 });
+
+const btnRemoveItems = document.getElementById("btnRemoveItems");
+btnRemoveItems.addEventListener("click", () => {
+    const Items = document.querySelectorAll(".itemList");
+    Items.forEach(item => item.remove());
+})
+
+// para selecionarmos todos os items com a mesma classe, devemos utilizar o .querySelectorAll(". (nome da classe)")
