2a18fac92cacc56fd26490d6318d0838020c2768
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-05-16 17:39:04 +0000

    Add li to ul with JS

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
-  document.getElementById("title").textContent = "Texto alterado!";
+  document.getElementById("title").textContent = "Text change";
 });
+
+const btnAdditems = document.getElementById("btnAdditems");
+btnAdditems.addEventListener("click", () => {
+    document.createElement("li");
+})
