4c59fa94d8c1f7b683ced3fe946b38d451edf275
Gabriel Ribeiro <gabrielribeirotnc@gmail.com>
2025-05-20 20:14:07 +0000

    add style to list

--- lista/script.js
+++ lista/script.js
@@ -1,6 +1,9 @@
 const addBtn = document.getElementById('addBtn');
 addBtn.addEventListener("click", () => {
 
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('itemWrapper');
+
     const li = document.createElement('li');
     li.classList.add('liItem');
     ul.appendChild(li);
@@ -11,7 +14,12 @@ addBtn.addEventListener("click", () => {
     li.appendChild(btnRemove);
 
     btnRemove.onclick = function () {
-        li.remove();
+        wrapper.remove();
     };
 
+    document.getElementById('txt').value = "";
+    li.appendChild(btnRemove);
+    wrapper.appendChild(li);
+    document.getElementById('ul').appendChild(wrapper);
 });
+
